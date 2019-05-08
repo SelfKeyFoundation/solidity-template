607f062ce693563ae19f6b6f019fbb72597d6580
Carlos Bruguera <cbruguera@gmail.com>
2019-05-08 17:09:07 +0700

    config files updates

--- truffle-config.js
+++ truffle-config.js
@@ -1,8 +1,17 @@
-// const HDWalletProvider = require('truffle-hdwallet-provider');
-// const infuraKey = "fj4jll3k.....";
-//
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".local/wallet").toString().trim();
+require('dotenv').config()
+
+const HDWalletProvider = require('truffle-hdwallet-provider')
+const fs = require('fs');
+
+const infuraKey = process.env["INFURA_KEY"]
+const mainnetURL = "https://mainnet.infura.io/v3/" + infuraKey
+const ropstenURL = "https://ropsten.infura.io/v3/" + infuraKey
+
+const walletPath = "./local/wallet.json"  // Should be JSON file where "mnemonic" is found
+const { mnemonic } = JSON.parse(fs.readFileSync(walletPath))
+
+const MainnetProvider = new HDWalletProvider(mnemonic, mainnetURL)
+const RopstenProvider = new HDWalletProvider(mnemonic, ropstenURL)
 
 module.exports = {
   networks: {
@@ -35,14 +44,15 @@ module.exports = {
 
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
-    // ropsten: {
-      // provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/${infuraKey}`),
-      // network_id: 3,       // Ropsten's id
-      // gas: 5500000,        // Ropsten has a lower block limit than mainnet
+    ropsten: {
+      provider: () => RopstenProvider,
+      network_id: 3,
+      gas: 5500000,
+      gasPrice: 5000000000, // 5 gwei
       // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
       // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
       // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-    // },
+    },
 
     // Useful for private networks
     // private: {
